b99e807915d22840f55e36f415c884921267d1d4
MrCode <MrCodeInc@users.noreply.github.com>
2016-09-07 19:11:39 -0400

    calc.py

---
+++ calc.py
@@ -0,0 +1,8 @@
+print "MrCodeInc's crappy first python progarm! YAY!"
+
+num1 = input("Please enter in the first number to be added:")
+num2 = input("Please enter in the second number to be added:")
+
+sum = num1 + num2
+
+print "The sum of the two numbers entered is: ", sum
